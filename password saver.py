7d655cc1765002f7c3c24d4f3de6e7dbcc1a147f
neon-coder <79017098+neon-coder@users.noreply.github.com>
2021-05-14 14:53:31 +0530

    Create password saver.py

---
+++ password saver.py
@@ -0,0 +1,25 @@
+#obsrerve the code and set your passwords you want
+while True:  
+    print("Hello sir")
+    type_password = input("Enter the password: ").strip().capitalize()
+    if type_password == "Write your password":
+        passwords = print("Here are the passwords")
+        print("""
+        keep your any email id and others....
+
+        """)
+
+    name = input("Enter your name: ")
+
+    while True:
+        if name == "Write your name":
+            enter = input("Please enter the password you want: ")
+#what you type which is there after the two equals to you will get the print
+        if enter == "your gmail...":
+            print("password")
+#you can set your passwords and gmail. continue the elif statment or add loop and dattabase to add other passwords
+        elif enter == "any other":
+            print("password")
+#this will keep your passwords safe that nobody can see your passwords without the entery password. if you don't keep else and quit it can print 'Please Enter the passwords you want' in the enter variable
+        else:
+            quit()
